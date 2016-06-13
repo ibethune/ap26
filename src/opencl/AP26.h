2481d6f1d0739e05218a7eeb0f3b58c95adb2199
Iain Bethune <ibethune@epcc.ed.ac.uk>
2016-06-13 22:04:21 +0100

    Add definition of OpenCL SearchAP26() function

---
+++ src/opencl/AP26.h
@@ -0,0 +1,286 @@
+
+/*
+	***************
+	Bryan Little May 2014
+	+ Using ~1.1 gigabytes RAM on device and host
+	+ This is code tuned for OpenCL devices.
+	+ Two 524mb n59 arrays because of OpenCL device memory allocation limits on AMD gpu
+
+*/
+void SearchAP26(int K, int SHIFT, int sieve_ls)
+{ 
+
+	int64_t STEP;
+	int64_t n0;
+	int64_t n43, n47, n53;
+	int i43, i47, i53;
+	int i3, i5, i31, i37, i41;
+	int64_t S31, S37, S41, S43, S47, S53, S59;
+        size_t global_size[2];
+        size_t local_size[2];
+
+	time_t total_start_time, total_finish_time;
+	time (&total_start_time);
+
+
+	STEP=K*PRIM23;
+	n0=(N0*(K%17835)+((N0*17835)%MOD)*(K/17835)+N30)%MOD;
+	S31=(PRES2*(K%17835)+((PRES2*17835)%MOD)*(K/17835))%MOD;
+	S37=(PRES3*(K%17835)+((PRES3*17835)%MOD)*(K/17835))%MOD;
+	S41=(PRES4*(K%17835)+((PRES4*17835)%MOD)*(K/17835))%MOD;
+	S43=(PRES5*(K%17835)+((PRES5*17835)%MOD)*(K/17835))%MOD;
+	S47=(PRES6*(K%17835)+((PRES6*17835)%MOD)*(K/17835))%MOD;
+	S53=(PRES7*(K%17835)+((PRES7*17835)%MOD)*(K/17835))%MOD;
+	S59=(PRES8*(K%17835)+((PRES8*17835)%MOD)*(K/17835))%MOD;
+
+	// note: n59MAX == 4808136200
+	// note: n53MAX == 137375320
+
+	int count=0;
+	int count2=0;
+
+	for(i31=0;i31<7;++i31)
+	for(i37=0;i37<13;++i37)
+	if(i37-i31<=10&&i31-i37<=4)
+	for(i41=0;i41<17;++i41)
+	if(i41-i31<=14&&i41-i37<=14&&i31-i41<=4&&i37-i41<=10)
+	for(i3=0;i3<2;++i3)
+	for(i5=0;i5<4;++i5){ 
+		n43=(n0+i3*S3+i5*S5+i31*S31+i37*S37+i41*S41)%MOD;
+		for(i43=(PRIME5-24);i43>0;i43--){
+			n47=n43;
+			for(i47=(PRIME6-24);i47>0;i47--){
+				n53=n47;
+				for(i53=(PRIME7-24);i53>0;i53--){
+					//n59=n53;
+					if(count < halfn59s){
+						n59_0_h[count]=n53;
+						count++;
+					}
+					else{
+						n59_1_h[count2]=n53;
+						count2++;
+					}
+					n53+=S53;
+					if(n53>=MOD)n53-=MOD;
+				}
+				n47+=S47;
+				if(n47>=MOD)n47-=MOD;
+			}
+			n43+=S43;
+			if(n43>=MOD)n43-=MOD;
+		}
+	}
+
+#ifdef BOINC
+	boinc_begin_critical_section();
+#endif
+	// offload to gpu
+	sclWrite(hardware, halfn59s * sizeof(int64_t), n59_0_d, n59_0_h);
+	sclWrite(hardware, halfn59s * sizeof(int64_t), n59_1_d, n59_1_h);
+
+	// offset kernel
+        global_size[0]=576; global_size[1]=1;
+        local_size[0]=64; local_size[1]=1;
+	sclSetKernelArg(offset, 0, sizeof(cl_mem), &offset_d);
+	sclEnqueueKernel(hardware, offset, global_size, local_size);
+	// end offset
+
+	sclFinish(hardware);
+
+#ifdef BOINC
+	boinc_end_critical_section();
+#endif
+
+	int STARTSHIFT;
+
+	for(STARTSHIFT=SHIFT; SHIFT<(STARTSHIFT+640); SHIFT+=64){
+
+		time_t start_time, finish_time;
+		time (&start_time);
+
+#ifdef BOINC
+		boinc_begin_critical_section();
+#endif
+
+		// clearok kernel
+		global_size[0]=23744; global_size[1]=1;
+		local_size[0]=64; local_size[1]=1;
+		sclSetKernelArg(clearok, 0, sizeof(cl_mem), &OK_d);
+		sclSetKernelArg(clearok, 1, sizeof(cl_mem), &OKOK_d);
+		sclEnqueueKernel(hardware, clearok, global_size, local_size);
+		// end clearok
+ 
+
+		// setok kernel
+		global_size[0]=576; global_size[1]=1;
+		local_size[0]=64; local_size[1]=1;
+		sclSetKernelArg(setok, 0, sizeof(int64_t), &STEP);
+		sclSetKernelArg(setok, 1, sizeof(int), &SHIFT);
+		sclSetKernelArg(setok, 2, sizeof(cl_mem), &OK_d);
+		sclSetKernelArg(setok, 3, sizeof(cl_mem), &offset_d);
+		sclEnqueueKernel(hardware, setok, global_size, local_size);
+		// end setok
+
+
+		// setupokok kernel
+		global_size[0]=576; global_size[1]=1;
+		local_size[0]=64; local_size[1]=1;
+		sclSetKernelArg(setupokok, 0, sizeof(int64_t), &STEP);
+		sclSetKernelArg(setupokok, 1, sizeof(int), &SHIFT);
+		sclSetKernelArg(setupokok, 2, sizeof(cl_mem), &OK_d);
+		sclSetKernelArg(setupokok, 3, sizeof(cl_mem), &OKOK_d);
+		sclSetKernelArg(setupokok, 4, sizeof(cl_mem), &offset_d);
+		sclEnqueueKernel(hardware, setupokok, global_size, local_size);
+		// end setupokok
+
+
+		// clearsol kernel
+		global_size[0]=1024; global_size[1]=1;
+		local_size[0]=64; local_size[1]=1;
+		sclSetKernelArg(clearsol, 0, sizeof(cl_mem), &sol_k_d);
+		sclSetKernelArg(clearsol, 1, sizeof(cl_mem), &solcount_d);
+		sclEnqueueKernel(hardware, clearsol, global_size, local_size);
+		// end clearsol
+
+		sclFinish(hardware);
+
+#ifdef BOINC
+		boinc_end_critical_section();
+#endif
+
+
+		// kernel config for sieve
+		const int worksize=1571840;	// must be divisible by 64 and 1024
+
+		int p;
+		// n59 array 0 checking
+		for( p=0; p<halfn59s; p+=worksize ){
+
+#ifdef BOINC
+			boinc_begin_critical_section();
+#endif
+
+			// sieve kernel
+			sclSetKernelArg(sieve, 0, sizeof(cl_mem), &n59_0_d);
+			sclSetKernelArg(sieve, 1, sizeof(int64_t), &S59);
+			sclSetKernelArg(sieve, 2, sizeof(int), &SHIFT);
+			sclSetKernelArg(sieve, 3, sizeof(cl_mem), &n_result_d);
+			sclSetKernelArg(sieve, 4, sizeof(cl_mem), &OKOK_d);
+			sclSetKernelArg(sieve, 5, sizeof(cl_mem), &ncount_d);
+			sclSetKernelArg(sieve, 6, sizeof(int), &p);
+			global_size[0]=worksize; global_size[1]=1;
+			local_size[0]=sieve_ls; local_size[1]=1;
+			sclEnqueueKernel(hardware, sieve, global_size, local_size);
+			// end sieve
+
+/*			int* numbern;
+			numbern = (int*)malloc(sizeof(int));
+			sclRead(hardware, sizeof(int), ncount_d, numbern);
+			printf("narray size: %d of max %d\n",numbern[0],numn);
+			free(numbern);
+*/
+			// checkn kernel
+			sclSetKernelArg(checkn, 0, sizeof(cl_mem), &n_result_d);
+			sclSetKernelArg(checkn, 1, sizeof(int64_t), &STEP);
+			sclSetKernelArg(checkn, 2, sizeof(cl_mem), &sol_k_d);
+			sclSetKernelArg(checkn, 3, sizeof(cl_mem), &sol_val_d);
+			sclSetKernelArg(checkn, 4, sizeof(cl_mem), &ncount_d);
+			sclSetKernelArg(checkn, 5, sizeof(cl_mem), &solcount_d);
+			global_size[0]=numn; global_size[1]=1;
+			local_size[0]=64; local_size[1]=1;
+			sclEnqueueKernel(hardware, checkn, global_size, local_size);
+			// end checkn
+
+			// call finish here to fix ctrl-c crash on AMD GPU
+			sclFinish(hardware);
+
+#ifdef BOINC
+			boinc_end_critical_section();
+#endif
+
+		}
+
+		// n59 array 1 checking
+		for( p=0; p<halfn59s; p+=worksize ){
+
+#ifdef BOINC
+			boinc_begin_critical_section();
+#endif
+
+			// sieve kernel
+			sclSetKernelArg(sieve, 0, sizeof(cl_mem), &n59_1_d);
+			sclSetKernelArg(sieve, 1, sizeof(int64_t), &S59);
+			sclSetKernelArg(sieve, 2, sizeof(int), &SHIFT);
+			sclSetKernelArg(sieve, 3, sizeof(cl_mem), &n_result_d);
+			sclSetKernelArg(sieve, 4, sizeof(cl_mem), &OKOK_d);
+			sclSetKernelArg(sieve, 5, sizeof(cl_mem), &ncount_d);
+			sclSetKernelArg(sieve, 6, sizeof(int), &p);
+			global_size[0]=worksize; global_size[1]=1;
+			local_size[0]=sieve_ls; local_size[1]=1;
+			sclEnqueueKernel(hardware, sieve, global_size, local_size);
+			// end sieve
+
+
+			// checkn kernel
+			sclSetKernelArg(checkn, 0, sizeof(cl_mem), &n_result_d);
+			sclSetKernelArg(checkn, 1, sizeof(int64_t), &STEP);
+			sclSetKernelArg(checkn, 2, sizeof(cl_mem), &sol_k_d);
+			sclSetKernelArg(checkn, 3, sizeof(cl_mem), &sol_val_d);
+			sclSetKernelArg(checkn, 4, sizeof(cl_mem), &ncount_d);
+			sclSetKernelArg(checkn, 5, sizeof(cl_mem), &solcount_d);
+			global_size[0]=numn; global_size[1]=1;
+			local_size[0]=64; local_size[1]=1;
+			sclEnqueueKernel(hardware, checkn, global_size, local_size);
+			// end checkn
+
+			// call finish here to fix ctrl-c crash on AMD GPU
+			sclFinish(hardware);
+
+#ifdef BOINC
+			boinc_end_critical_section();
+#endif
+
+		}
+
+
+#ifdef BOINC
+		boinc_begin_critical_section();
+#endif
+
+		// copy n results and n count to local memory
+		sclRead(hardware, sol * sizeof(int), sol_k_d, sol_k_h);
+		sclRead(hardware, sol * sizeof(int64_t), sol_val_d, sol_val_h);
+
+		sclFinish(hardware);
+
+		int e=0;
+		while(sol_k_h[e] != 0){
+			ReportSolution(sol_k_h[e],K,sol_val_h[e]);
+
+			/* 	hash for BOINC quorum
+				we create a hash based on each AP10+ result mod 1000
+				and that result's AP length  	*/
+			result_hash += sol_val_h[e] % 1000;
+			result_hash += sol_k_h[e];
+			if(result_hash > MAXINTV){
+				result_hash -= MAXINTV;
+			}
+	
+			e++;
+		}
+
+#ifdef BOINC
+		boinc_end_critical_section();
+#endif
+
+		printf("Computation for K: %d SHIFT: %d complete.\n", K, SHIFT);
+
+		time(&finish_time);
+		printf("GPU time was %d seconds\n", (int)finish_time - (int)start_time);
+	}
+
+	time(&total_finish_time);
+	printf("total GPU time for K was %d seconds\n", (int)total_finish_time - (int)total_start_time);
+
+}
