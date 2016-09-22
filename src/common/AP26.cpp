f1c9b683a9f3a72125dc9ec68d81e09e11f79a6f
Iain Bethune <ibethune@epcc.ed.ac.uk>
2016-09-22 23:13:43 +0100

    Updates for new build env on Mac OS X 10.12

--- src/common/AP26.cpp
+++ src/common/AP26.cpp
@@ -181,8 +181,8 @@ sclHard sclGetBOINCHardware( int argc, char** argv ) {
 	fprintf(stderr, "GPU Info:\n  Platform name: %s\n  Vendor: %s\n  Device name: %s\n", platformName, platformVendor, deviceName );
         int64_t gmem = (int64_t)_sclGetMaxGlobalMemSize(hardware.device);
         int64_t maxalloc = (int64_t)_sclGetMaxMemAllocSize(hardware.device);
-        fprintf(stderr, "  GPU RAM:        %"INT64_FORMAT"\n", gmem);
-        fprintf(stderr, "  GPU max malloc: %"INT64_FORMAT"\n", maxalloc);
+        fprintf(stderr, "  GPU RAM:        %" INT64_FORMAT "\n", gmem);
+        fprintf(stderr, "  GPU max malloc: %" INT64_FORMAT "\n", maxalloc);
 
 	clRetainCommandQueue( hardware.queue );
 	clRetainContext( hardware.context );
@@ -310,7 +310,7 @@ static void write_hash()
 
 	int64_t hash = (int64_t)( (minmax << 32) | solhash);
 
-//	printf("minmax: %llu solhash: %u\ndechash: %"INT64_FORMAT"\n", minmax, solhash, hash);
+//	printf("minmax: %llu solhash: %u\ndechash: %" INT64_FORMAT "\n", minmax, solhash, hash);
 
 	// convert to hex
 	while(hash!=0){
@@ -556,7 +556,7 @@ void ReportSolution(int AP_Length,int difference,int64_t First_Term)
 
 	if (i < AP_Length){
 
-		printf("Non-Solution: %d %d %"INT64_FORMAT"\n",AP_Length,difference,First_Term);
+		printf("Non-Solution: %d %d %" INT64_FORMAT "\n",AP_Length,difference,First_Term);
 
 		if (val_base2_ap26(AP_Length,difference,First_Term) < AP_Length){
 			// GPU really did calculate something wrong.  It's not a prp base 2 AP
@@ -578,14 +578,14 @@ void ReportSolution(int AP_Length,int difference,int64_t First_Term)
 		if (results_file == NULL)
 			results_file = my_fopen(RESULTS_FILENAME,"a");
 
-		printf("Solution: %d %d %"INT64_FORMAT"\n",AP_Length,difference,First_Term);
+		printf("Solution: %d %d %" INT64_FORMAT "\n",AP_Length,difference,First_Term);
 
 		if (results_file == NULL){
 			fprintf(stderr,"Cannot open %s !!!\n",RESULTS_FILENAME);
 			exit(EXIT_FAILURE);
 		}
 
-		if (fprintf(results_file,"%d %d %"INT64_FORMAT"\n",AP_Length,difference,First_Term)<0){
+		if (fprintf(results_file,"%d %d %" INT64_FORMAT "\n",AP_Length,difference,First_Term)<0){
 			fprintf(stderr,"Cannot write to %s !!!\n",RESULTS_FILENAME);
 			exit(EXIT_FAILURE);
 		}
